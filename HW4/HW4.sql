ece195b86cab89606ae6587cbf8187c2c8232b8c
Precioux <uni.mahdipour@gmail.com>
2023-04-22 20:18:38 +0330

    HW4

---
+++ HW4/HW4.sql
@@ -0,0 +1,13 @@
+CREATE PROCEDURE spCalculateClassStatus
+AS
+BEGIN
+  SELECT 
+    CASE 
+      WHEN COUNT(CASE WHEN stuGrade < 10 THEN 1 END) <= 1 THEN 'GOOD'
+      WHEN COUNT(CASE WHEN stuGrade < 10 THEN 1 END) BETWEEN 2 AND 3 THEN 'Normal'
+      ELSE 'Bad'
+    END AS ClassStatus
+  FROM tblstudent;
+END
+
+EXEC spCalculateClassStatus;
